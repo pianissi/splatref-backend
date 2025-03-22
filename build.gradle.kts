e7756da1ea81c125c6ade7dacffd50b5958265e1
pianissi <132866503+pianissi@users.noreply.github.com>
2025-03-22 20:57:37 +1100

    packages: add spring security + jwt

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,11 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("commons-io:commons-io:+")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	testImplementation("org.springframework.security:spring-security-test")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 tasks.withType<Test> {
